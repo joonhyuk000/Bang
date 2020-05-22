acadb958be025b180eade963d59b21effed595be
DATA8320-12 <DATA8320-12@DESKTOP-GJM25HJ>
2020-05-22 11:39:36 +0900

    5/22

---
+++ JavaPrj/src/tndjq.java
@@ -0,0 +1,16 @@
+
+public class tndjq {
+
+	public static void main(String[] args) {
+
+		int i;
+		
+		for(i=1; i<=100; i++) {
+			System.out.printf("for문을 %d회 실행했습니다.\n",i);
+			if(i==57)
+			break;
+			}
+		System.out.printf("for문을 종료했습니다.\n");
+	}
+
+}
