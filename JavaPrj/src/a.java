8e3e5cda0be43dafb885a5757ff07509a2a318a4
DATA8320-12 <joonhyuk000@naver.com>
2020-06-02 17:17:36 +0900

    6/2 잔디심기

---
+++ JavaPrj/src/a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class a{
+    public static void main(String[] args){
+    	Scanner scn = new Scanner(System.in);
+    	int a;
+    	int b;
+    	a=scn.nextInt();
+    	b=scn.nextInt();
+    	System.out.println(a+b);
+    }
+}
