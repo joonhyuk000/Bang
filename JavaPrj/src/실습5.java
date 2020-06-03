b09ac4457e74612cb883af2f77976660d3b85946
DATA8320-12 <joonhyuk000@naver.com>
2020-06-03 12:45:53 +0900

    6/3 잔디심기

--- JavaPrj/src/실습5.java
+++ JavaPrj/src/실습5.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+public class 실습5{
+    public static void main (String[] args){
+    
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    
+    if(a>0 && b>0)
+    System.out.println("1");
+    if(a<0 && b>0)
+    System.out.println("2");
+    if(a<0 && b<0)
+    System.out.println("3");
+    if(a>0 && b<0)
+    System.out.println("4");
+    
 
-public class 실습5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    System.out.println("");
+    }
 }
