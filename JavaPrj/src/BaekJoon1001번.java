1c692beb37103fa04acb38be39b1362f75084a50
DATA8320-12 <joonhyuk000@naver.com>
2020-06-04 10:06:01 +0900

    6/4 잔디심기

---
+++ JavaPrj/src/BaekJoon1001번.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaekJoon1001¹ø {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a;
+		int b;
+		a=s.nextInt();
+		b=s.nextInt();
+		System.out.println(a-b);
+	}
+
+}
