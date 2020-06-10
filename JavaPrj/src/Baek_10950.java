a00c6bc244a24652554cd1d4d25ac8fc7fc41383
DATA8320-12 <joonhyuk000@naver.com>
2020-06-10 12:44:20 +0900

    6/10 잔디심기

---
+++ JavaPrj/src/Baek_10950.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Baek_10950{
+
+	public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        for(int b=0;b<a;b++) {
+        	int c=s.nextInt();
+        	int d=s.nextInt();
+       System.out.println(c+d);        	
+        	}
+        s.close();
+		}
+	}
