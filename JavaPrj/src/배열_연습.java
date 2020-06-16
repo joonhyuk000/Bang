2da915e746d0957a34bb174ca9a9dfb1e14279ce
DATA8320-12 <joonhyuk000@naver.com>
2020-06-16 13:49:59 +0900

    6/16 잔디심기

---
+++ JavaPrj/src/배열_연습.java
@@ -0,0 +1,24 @@
+
+public class 배열_연습 {
+
+	public static void main(String[] args) {
+
+		int j= 11;
+		int hap=0;
+		int [] num=new int[90];
+		for(int i=0; i<=89; i++) {
+			num[i] = j;
+			hap += num[i];
+			j++;
+		}
+		int l=11;
+		int k=0;
+		while(l<=100) {
+			System.out.printf("num[%d] = %d\n", k, l);
+			l++;
+			k++;
+		}
+		System.out.println(hap);
+	}
+
+}
